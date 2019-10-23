31243e9ce15f4086e93a2cd1d97e7813c26563e9
ahyunlee5 <ahyunlee@umich.edu>
2019-10-22 20:12:18 -0700

    revision

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 function clickInfoIcon() {
   $('.infoIcon').on('click', function() {
+    $('#projects').slideUp();
     return $('#aboutMe').slideToggle('hide');
     // console.log('click info');
   });
@@ -9,6 +10,7 @@ function clickInfoIcon() {
 
 function clickProjectIcon() {
   $('.projectIcon').on('click', function() {
+    $('#aboutMe').slideUp();
     return $('#projects').slideToggle('hide');
     // console.log('click project');
   });
@@ -20,3 +22,15 @@ function clickables () {
 }
 
 $(clickables());
+// function clickIcon () {
+//   $('.icon').on('click', function(event) {
+//     const targetIcon = $(event.currentTarget);
+//     const notTargetIcon = $('.icon').not(targetIcon);
+//     notTargetIcon.slideToggle('hide');
+//     targetIcon.slideToggle('hide');
+
+//     console.log('working?');
+//   });
+// }
+
+// $(clickIcon);
